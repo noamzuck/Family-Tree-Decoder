9c357b030e15209e81a12a1117e4ab68e0f09472
noamzuck <62641593+noamzuck@users.noreply.github.com>
2023-02-02 19:23:06 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,63 +1,78 @@
 var list = {};
 var gens = {};
 var tree;
+var treeData;
+
+if(localStorage.getItem("dataUrlFileFamTree")){
+    document.getElementById("browse").style.display = 'none';
+    treeData = JSON.parse(localStorage.getItem("dataUrlFileFamTree"));
+    process();
+}
 
 document.getElementById("picker").addEventListener('change', async function(e) {
     document.getElementById("browse").style.display = 'none';
-    let url = URL.createObjectURL(e.target.files[0]);
-    treeJson = d3.json(/*"data.json"*/url, function(error, treeData) {
-        tree = dTree.init(treeData,
-                    {
-                        target: "#graph",
-                        debug: true,
-                        hideMarriageNodes: true,
-                        marriageNodeSize: 5,
-                        height: 800,
-                        width: 1200,
-                        callbacks: {
-                            nodeClick: function(name, extra) {
-                                alert('Click: ' + name);
-                            },
-                            nodeRightClick: function(name, extra) {
-                                alert('Right-click: ' + name);
-                            },
-                            textRenderer: function(name, extra, textClass) {
-                                if (extra && extra.nickname)
-                                    name = name + " (" + extra.nickname + ")";
-                                if(extra && extra.Hebrewname){
-                                    if(extra.Hebrewname == 'null') hName = '';
-                                    else hName = "<br><p align='center' class='descibe'>" + extra.Hebrewname + "</p>";
-                                } else hName = '';
-                                if(extra && extra.Dates){
-                                    if(extra.Dates == 'null') Dates = '';
-                                    else Dates = "<br><p align='center' class='dates'>" + extra.Dates + "</p>";
-                                } else Dates = '';
-                                if(extra && extra.Gener) Gen = "<div style='display: flex; justify-content: center'><div class='circle'>" + extra.Gener + "</div></div>";
-                                else Gen = '';
-                                return Gen + "<br><p align='center' class='" + textClass + "'>" + name + "</p>" + hName + Dates;
-                            },
-                            nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
-                                list[name] = id;
-                                gens[id] = extra.Gener;
-                                let node = '';
-                                  node += '<div ';
-                                  node += 'style="height:100%;width:100%;" ';
-                                  node += 'class="' + nodeClass + '" ';
-                                  node += 'id="node' + id + '">\n';
-                                  node += textRenderer(name, extra, textClass);
-                                  node += '</div>';
-                                  return node;
-                            },
-                            marriageClick: function(extra, id) {
-                                alert('Clicked marriage node' + id);
-                            },
-                            marriageRightClick: function(extra, id) {
-                                alert('Right-clicked marriage node' + id);
-                            },
-                        }
-                    });
+    let selected = e.target.files[0];
+    let reader = new FileReader();
+    reader.addEventListener("loadend", () => {
+        treeData = JSON.parse(reader.result);
+        localStorage.setItem("dataUrlFileFamTree", reader.result);
+        process();
     });
+    reader.readAsText(selected);
 });
+
+function process(){
+    tree = dTree.init(treeData,
+        {
+            target: "#graph",
+            debug: true,
+            hideMarriageNodes: true,
+            marriageNodeSize: 5,
+            height: 800,
+            width: 1200,
+            callbacks: {
+                nodeClick: function(name, extra) {
+                    alert('Click: ' + name);
+                },
+                nodeRightClick: function(name, extra) {
+                    alert('Right-click: ' + name);
+                },
+                textRenderer: function(name, extra, textClass) {
+                    if (extra && extra.nickname)
+                        name = name + " (" + extra.nickname + ")";
+                    if(extra && extra.Hebrewname){
+                        if(extra.Hebrewname == 'null') hName = '';
+                        else hName = "<br><p align='center' class='descibe'>" + extra.Hebrewname + "</p>";
+                    } else hName = '';
+                    if(extra && extra.Dates){
+                        if(extra.Dates == 'null') Dates = '';
+                        else Dates = "<br><p align='center' class='dates'>" + extra.Dates + "</p>";
+                    } else Dates = '';
+                    if(extra && extra.Gener) Gen = "<div style='display: flex; justify-content: center'><div class='circle'>" + extra.Gener + "</div></div>";
+                    else Gen = '';
+                    return Gen + "<br><p align='center' class='" + textClass + "'>" + name + "</p>" + hName + Dates;
+                },
+                nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
+                    list[name] = id;
+                    gens[id] = extra.Gener;
+                    let node = '';
+                      node += '<div ';
+                      node += 'style="height:100%;width:100%;" ';
+                      node += 'class="' + nodeClass + '" ';
+                      node += 'id="node' + id + '">\n';
+                      node += textRenderer(name, extra, textClass);
+                      node += '</div>';
+                      return node;
+                },
+                marriageClick: function(extra, id) {
+                    alert('Clicked marriage node' + id);
+                },
+                marriageRightClick: function(extra, id) {
+                    alert('Right-clicked marriage node' + id);
+                },
+            }
+        });
+}
 
 function search(word){
     let searchVal = word;
